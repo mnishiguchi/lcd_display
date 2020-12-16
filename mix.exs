f727c15f0b1700cc62bb00d3df7fb92bddb66d26
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-12-16 16:53:59 -0500

    Delete the application file which is unneeded

--- mix.exs
+++ mix.exs
@@ -21,10 +21,7 @@ defmodule LcdDisplay.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger],
-      mod: {LcdDisplay.Application, []}
-    ]
+    []
   end
 
   # ensure test/support is compiled
